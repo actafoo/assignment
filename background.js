6a0b5af563ff3fa6cbc5a742dd8ba0f3bef7d95e
지주선(JUSUN JI) <wlwntjsdm@naver.com>
2023-02-06 00:13:04 +0900

    Add files via upload

---
+++ background.js
@@ -0,0 +1,11 @@
+const images = ['0.jpg', '1.jpg', '2.jpg'];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+console.log(chosenImage);
+// const bgImage = document.createElement('img');
+
+// bgImage.src = `${chosenImage}`;
+
+// document.body.appendChild(bgImage);
+// bgImage.style.width = '100%';
